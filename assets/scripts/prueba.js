477e36535067164ab050a0e233cb2e101bbb0bc1
CrimvaelVR <maikelvr2004@gmail.com>
2023-07-16 20:39:22 -0400

    Creacion de Variables
    las variables de referencia al dom

---
+++ assets/scripts/prueba.js
@@ -0,0 +1,12 @@
+// Obtener los elementos del DOM
+const studentUL = document.getElementById("student-ul");
+const addBtn = document.getElementById("add-btn");
+const modal = document.getElementById("modal");
+const modalTitle = document.getElementById("modal-title");
+const nameInput = document.getElementById("name");
+const surnameInput = document.getElementById("surname");
+const courseInput = document.getElementById("course");
+const addressInput = document.getElementById("address");
+const descriptionInput = document.getElementById("description");
+const saveBtn = document.getElementById("save-btn");
+const cancelBtn = document.getElementById("cancel-btn");
