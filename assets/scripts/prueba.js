8830fa3d8ed4f1220dcfdfcb334f68669c9e133f
CrimvaelVR <maikelvr2004@gmail.com>
2023-07-16 20:40:04 -0400

    Función pa guardar los datos pt1
    Es la funcion que permite guardar los datos

--- assets/scripts/prueba.js
+++ assets/scripts/prueba.js
@@ -10,3 +10,27 @@ const addressInput = document.getElementById("address");
 const descriptionInput = document.getElementById("description");
 const saveBtn = document.getElementById("save-btn");
 const cancelBtn = document.getElementById("cancel-btn");
+
+// Función para guardar los datos en LocalStorage
+function saveStudent(name, surname, course, address, description) {
+    let students = JSON.parse(localStorage.getItem("students")) || [];
+    students.push({
+      name: name,
+      surname: surname,
+      course: course,
+      address: address,
+      description: description,
+    });
+    localStorage.setItem("students", JSON.stringify(students));
+  
+    // Mostrar mensaje de éxito
+    const successMessage = document.createElement("div");
+    successMessage.textContent = "Agregado exitosamente";
+    successMessage.classList.add("exito");
+    document.body.appendChild(successMessage);
+  
+    // Desvanecer el mensaje después de 3 segundos
+    setTimeout(() => {
+      successMessage.remove();
+    }, 3000);
+  }
