ffbd8992c2ce84ecaee8df1791ba44bd8d0f90ce
CrimvaelVR <maikelvr2004@gmail.com>
2023-07-16 20:43:46 -0400

    Función para borrar estudiante
    ESta función permite borrar un estudiante, verificar si desea hacerlo y mandar nensaje de realizado

--- assets/scripts/prueba.js
+++ assets/scripts/prueba.js
@@ -95,3 +95,49 @@ function editStudent(index) {
     saveBtn.setAttribute("data-id", index);
     modal.style.display = "block";
   }
+
+  // Función para eliminar un estudiante
+function deleteStudent(index) {
+    let students = JSON.parse(localStorage.getItem("students")) || [];
+  
+    // Crear un elemento HTML para el cuadro de diálogo
+    const dialog = document.createElement("div");
+    dialog.classList.add("dialog");
+    dialog.innerHTML = `
+      <p>¿Está seguro de que desea eliminar al estudiante?</p>
+      <button class="btn-ok">Aceptar</button>
+      <button class="btn-cancel">Cancelar</button>
+    `;
+  
+    // Agregar el cuadro de diálogo al DOM
+    document.body.appendChild(dialog);
+  
+    // Agregar un oyente para el evento de clic en los botones "Aceptar" y "Cancelar"
+    dialog.addEventListener("click", (event) => {
+      if (event.target.classList.contains("btn-ok")) {
+        // Obtener el elemento li del estudiante que se va a eliminar
+        const li = studentUL.querySelector(`li:nth-child(${index + 1})`);
+  
+        // Agregar la clase slide-out para la animación de deslizamiento
+        li.classList.add("slide-out");
+  
+        // Retrasar la eliminación hasta que se complete la animación
+        setTimeout(() => {
+          students.splice(index, 1);
+          localStorage.setItem("students", JSON.stringify(students));
+          loadStudents();
+        }, 500); // 500 milisegundos es igual al tiempo de duración de la animación
+  
+        // Eliminar el elemento li del DOM después de que se complete la animación
+        setTimeout(() => {
+          li.remove();
+        }, 1000); // 1000 milisegundos es igual al tiempo de duración de la animación más un pequeño retraso
+  
+        // Eliminar el cuadro de diálogo del DOM
+        dialog.remove();
+      } else if (event.target.classList.contains("btn-cancel")) {
+        // Eliminar el cuadro de diálogo del DOM
+        dialog.remove();
+      }
+    });
+  }
